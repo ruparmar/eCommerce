db8895b51fd57c638adda6953847724cc449c92a
ruparmar <78189191+ruparmar@users.noreply.github.com>
2021-01-29 13:07:58 +0530

    Update action.js

--- src/actions/action.js
+++ src/actions/action.js
@@ -2,13 +2,30 @@ import { acttype } from '../data/data';
 import ApiService from '../services';
 import { config } from '../data/config';
 
+export const addToCartAct = (data)=>{
+    return (dispatch)=>{
+        dispatch({type:acttype.ADD_TO_CART,payload:data})
+    }
+}
+
+export const setApiData = (data) => {
+    return (dispatch) => {
+        dispatch({type:acttype.ADD_API_DATA, payload:data})
+    }
+}
+
 export const getApiData = ()=>{
     const url = config.host+config.endpoints.book;        
         let data=[]; 
     return (dispatch)=>{
         ApiService.apiCall('get',url,data, (res)=>{ 
-            dispatch({type:acttype.ADD_API_DATA,payload:res.data})
+            dispatch({type:acttype.ADD_API_DATA, payload:res.data})
         } )
-       
     }
 } 
+
+export const removeOrder = ()=>{
+    return (dispatch)=>{
+        dispatch({type:acttype.REMOVE_TO_ORDER})
+    }
+}
